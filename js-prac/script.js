d254a776eed0079d984ea378de2fb439a074e22c
Arseny <arsenii.adamyan@gmail.com>
2021-10-25 17:10:48 +0700

    prac 2

--- js-prac/script.js
+++ js-prac/script.js
@@ -15,5 +15,29 @@ for (let i = 0; i < 2; i++) {
     'Один из последних просмотренных фильмов?'
   );
   let qSecond = prompt('На сколько оцените его?');
-  personalMovieDB.movies[qFirst] = qSecond;
+
+  if (
+    qFirst != '' &&
+    qFirst != null &&
+    qSecond != '' &&
+    qSecond != null &&
+    qFirst.length < 50
+  ) {
+    personalMovieDB.movies[qFirst] = qSecond;
+  } else {
+    i--;
+  }
+}
+
+if (personalMovieDB.count > 0 && personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (
+  personalMovieDB.count > 10 &&
+  personalMovieDB.count < 30
+) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  alert('Вы киноман');
+} else {
+  alert('Произошла ошибка');
 }
